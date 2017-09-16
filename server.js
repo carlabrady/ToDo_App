a0755a16bf9612a228ca4dea8fb1a7e4f861bc9c
Carla Brady <carlakaybrady@gmail.com>
2017-09-16 04:47:30 -0500

    server spin up

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var indexRouter = require('./routes/index');
+var tasksRouter = require('./routes/tasks');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.use('/', indexRouter);
+// app.use('/tasks', tasksRouter);
+
+app.listen(3000, function() {
+    console.log('listening on 3000');
+});
